20ade55a5f599f968d40d03c309e33ee296f5cb7
s230137 <124596650+s230137@users.noreply.github.com>
2023-03-30 09:39:50 +0200

    Update Step1.jl

--- Assignment 2/Step1.jl
+++ Assignment 2/Step1.jl
@@ -26,11 +26,16 @@ model = Model(Gurobi.Optimizer)
 @variable(model, delta_more[t in 1:24, w in 1:seenScenarios] >= 0)
 @variable(model, delta_less[t in 1:24, w in 1:seenScenarios] >= 0)
 
-
 @objective(model, Max, sum(prob * sum(price_DA[t,w]*(p_DA[t] + 0.9*delta_more[t,w] - 1.2*delta_less[t,w]) for w in 1:seenScenarios) for t in 1:24))
 
 @constraint(model, cst1[t in 1:24], p_DA[t] <= P_nom)
 @constraint(model, cst2[t in 1:24, w in 1:seenScenarios], delta[t,w] == P_real[t,w] - p_DA[t])
 @constraint(model, cst3[t in 1:24, w in 1:seenScenarios], delta[t,w] == delta_more[t,w] - delta_less[t,w])
+@constraint(model, cst4[t in 1:24, w in 1:seenScenarios], delta_more[t,w] <= delta[t,w])
 
 optimize!(model)
+
+# value.(p_DA)
+# value.(delta)
+# value.(delta_more)
+# value.(delta_less)
