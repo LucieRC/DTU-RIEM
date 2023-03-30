308c58b81d76b3e7c603b19a3020a45d5c8efdae
s230137 <124596650+s230137@users.noreply.github.com>
2023-03-30 09:14:21 +0200

    Update Step1.jl

--- Assignment 2/Step1.jl
+++ Assignment 2/Step1.jl
@@ -1,24 +1,36 @@
-""" INPUTS
-"""
+# Step 1
+
+using Pkg, Gurobi, JuMP, Cbc, CSV, DataFrames, Random
+
+P_real_raw = CSV.read("C:/Users/mscuc/Downloads/inputs_wind.csv", DataFrame, delim=",")
+price_raw = CSV.read("C:/Users/mscuc/Downloads/inputs_price.csv", DataFrame, delim=",")
+system_raw = CSV.read("C:/Users/mscuc/Downloads/inputs_system.csv", DataFrame, delim=",")
+
+Random.seed!(69)
+scenarioOrdered = randperm(600)
+
+p_real = P_real_raw[:, scenarioOrdered]
+price_DA = price_raw[:, scenarioOrdered]
+system = system_raw[:, scenarioOrdered]
 
 
-
-
-
-
-LAMBDA = pass #dim 2
-PROBA = 1/600
+seenScenarios = 200
+unseenScenarios = 400
+prob = 1/seenScenarios
 P_nom = 150
-P_real = pass #dim2
+
+model = Model(Gurobi.Optimizer)
 
 @variable(model, p_DA[t in 1:24] >= 0)
-@variable(model, delta[w in 1:600, t in 1:24])
-@variable(model, delta_more[w in 1:600, t in 1:24] >= 0)
-@variable(model, delta_less[w in 1:600, t in 1:24] >= 0)
+@variable(model, delta[t in 1:24, w in 1:seenScenarios])
+@variable(model, delta_more[t in 1:24, w in 1:seenScenarios] >= 0)
+@variable(model, delta_less[t in 1:24, w in 1:seenScenarios] >= 0)
 
 
-@objective(model, Max, sum(sum(PROBA*(LAMBDA[w,t]*(p_DA[t]+0.9*delta_more[w,t]-1.2*delta_less[w,t]) for w in 1:600) for t in 1:24))
+@objective(model, Max, sum(prob * sum(price_DA[t,w]*(p_DA[t] + 0.9*delta_more[t,w] - 1.2*delta_less[t,w]) for w in 1:seenScenarios) for t in 1:24))
 
-@constraint(model, cst1[t], p_DA[t] <= P_nom)
-@constraint(model, cst2[w,t], delta[w,t] == P_real[w,t] - p_DA[t])
-@constraint(model, cst3[w,t], delta[w,t] == delta_more[w,t] - delta_less[w,t])
+@constraint(model, cst1[t in 1:24], p_DA[t] <= P_nom)
+@constraint(model, cst2[t in 1:24, w in 1:seenScenarios], delta[t,w] == P_real[t,w] - p_DA[t])
+@constraint(model, cst3[t in 1:24, w in 1:seenScenarios], delta[t,w] == delta_more[t,w] - delta_less[t,w])
+
+optimize!(model)
