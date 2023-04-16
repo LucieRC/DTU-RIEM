4d3d7de61ac49fc38bcbca6224865304afc90e75
LucieRC <lricqpro@outlook.fr>
2023-04-16 15:45:39 +0300

    1.3 1 price and 1.4 alpha varying

--- Assignment 2/Step1_3_1price.jl
+++ Assignment 2/Step1_3_1price.jl
@@ -28,27 +28,28 @@ P_nom = 150
 # beta = 0.1*collect(0:10)
 # vector = zeros(Float64, length(beta), 2)
 
-function sensitivity_analysis(coef1, coef2)
-    function compute_bal_part(t,w)
-        return system[t,w]*(coef1*price_DA[t,w]*delta_more[t,w] - 1*price_DA[t,w]*delta_less[t,w]) + (1-system[t,w])*(1*price_DA[t,w]*delta_more[t,w] - coef2*price_DA[t,w]*delta_less[t,w])
+function sensitivity_analysis_2prices(coef1, coef2)
+    #Fill the balancing price based on the system
+    for w in 1:600
+        for t in 1:24
+            if system[t,w] == 1
+                price_Bal[t,w] = coef1*price_DA[t,w]
+            end
+            if system[t,w] == 0
+                price_Bal[t,w] = coef2*price_DA[t,w]
+            end
+        end
     end
 
     model = Model(Gurobi.Optimizer)
-
     @variable(model, p_DA[t in 1:24] >= 0)
     @variable(model, delta[t in 1:24, w in 1:in_sample_scen])
-    @variable(model, delta_more[t in 1:24, w in 1:in_sample_scen] >= 0)
-    @variable(model, delta_less[t in 1:24, w in 1:in_sample_scen] >= 0)
-
-    @objective(model, Max, sum(prob * sum(price_DA[t,w]*p_DA[t] + compute_bal_part(t,w)    
-                                for w in 1:in_sample_scen) for t in 1:24))
-
+    # @variable(model, zeta)
+    # @variable(model, eta[w in 1:in_sample_scen] >= 0)
+    @objective(model, Max, sum(prob * sum(price_DA[t,w]*p_DA[t] + price_Bal[t,w]*delta[t,w] for w in 1:in_sample_scen) for t in 1:24))
     @constraint(model, cst1[t in 1:24], p_DA[t] <= P_nom)
     @constraint(model, cst2[t in 1:24, w in 1:in_sample_scen], delta[t,w] == p_real[t,w] - p_DA[t])
-    @constraint(model, cst3[t in 1:24, w in 1:in_sample_scen], delta[t,w] == delta_more[t,w] - delta_less[t,w])
-    @constraint(model, cst4[t in 1:24, w in 1:in_sample_scen], delta_more[t,w] <= P_nom)
-    @constraint(model, cst5[t in 1:24, w in 1:in_sample_scen], delta_less[t,w] <= P_nom)
-
+    # @constraint(model, cst3[w in 1:in_sample_scen], -sum(price_DA[t,w]*p_DA[t] + price_Bal[t,w]*delta[t,w] for t in 1:24) + zeta - eta[w] <= 0)
     optimize!(model)
 
     return(objective_value(model)) 
@@ -60,21 +61,21 @@ coef2 = 1.2
 to_store = ["coef1" "coef2" "obj_function"]
 print(to_store)
 for coef1 in 0.01*range(85,95)
-    obj = sensitivity_analysis(coef1,coef2)
+    obj = sensitivity_analysis_2prices(coef1,coef2)
     to_store = vcat(to_store, [coef1 coef2 obj])
     print(to_store)
     # vector[count,:] = [risk, obj]
 end
-CSV.write("outputs/step_1_3_coef1.csv", Tables.table(to_store))
+CSV.write("outputs/step_1_3_1price_coef1.csv", Tables.table(to_store))
 
 # SECOND SENSITIVITY ANALYSIS
 coef1 = 0.9
 to_store = ["coef1" "coef2" "obj_function"]
 print(to_store)
 for coef2 in 0.1*range(11,13,11)
-    obj = sensitivity_analysis(coef1,coef2)
+    obj = sensitivity_analysis_2prices(coef1,coef2)
     to_store = vcat(to_store, [coef1 coef2 obj])
     print(to_store)
     # vector[count,:] = [risk, obj]
 end
-CSV.write("outputs/step_1_3_coef2.csv", Tables.table(to_store))
+CSV.write("outputs/step_1_3_1price_coef2.csv", Tables.table(to_store))
